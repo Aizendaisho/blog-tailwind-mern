6320933ffe7ac6ba1a676730e63de071cdbc989a
Aizendaisho <96481537+Aizendaisho@users.noreply.github.com>
2023-01-30 23:30:44 -0400

    Api completa

--- api/index.js
+++ api/index.js
@@ -2,14 +2,41 @@ const express = require("express")
 const dotenv = require("dotenv")
 const app = express()
 const mongoose = require("mongoose")
-dotenv.config()
-const port = 8000
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+const port = 8000;
+dotenv.config();
 
-mongoose.set('strictQuery', true)
-mongoose.connect(process.env.MONGO_URL)
-  .then(() => console.log('Se conecto a la base de datos!'))
-  .catch((err)=> console.log(err));
+app.use(express.json());
 
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Se conecto a la base de datos!"))
+  .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("se ha subido el archivo");
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 
 
